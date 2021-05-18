a84b08032b12187ba55ebc09589e9cdd570f558e
ollonis <83023165+ollonis@users.noreply.github.com>
2021-05-18 10:52:14 -0500

    Update and rename index.py to signup.py

---
+++ signup.py
@@ -0,0 +1,9 @@
+#Username 
+username = input('Please enter your username here: ')
+user = username
+print('username stored..')
+#Password
+password = input('Please enter your password here: ')
+passw = password
+print('password stored..')
+#confirmation prompt
