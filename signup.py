116b2cdca940f9948abae21ca831e4c9b0a84004
ollonis <83023165+ollonis@users.noreply.github.com>
2021-05-18 11:56:54 -0500

    Update signup.py

--- signup.py
+++ signup.py
@@ -1,9 +1,11 @@
-#Username 
-username = input('Please enter your username here: ')
-user = username
-print('username stored..')
-#Password
-password = input('Please enter your password here: ')
-passw = password
-print('password stored..')
-#confirmation prompt
+#Username Prompt
+usr = input ('username: ')
+#Stores the username as a variable
+username = usr
+#Prompts password
+passwrd = input
+#Encrypts password
+key = Fernet.generate_key()
+encryption_type = Fernet(key)
+encrypted_message = encryption_type.encrypt(b + usrname)
+print(encrypted_message)
